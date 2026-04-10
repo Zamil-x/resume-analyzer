016ad4d3783322a57bf1eab59f564a0bd3a7eece
zamil-x <zamilapx@gmail.com>
2026-04-10 18:32:31 +1100

    new update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 
@@ -11,8 +12,31 @@ def upload():
     file = request.files['resume']
     
     if file:
-        filename = file.filename
-        return f"File uploaded: {filename}"
+        reader = PdfReader(file)
+        text = ""
+
+        for page in reader.pages:
+            text += page.extract_text()
+
+        # 🔍 skill check
+        skills = ["python", "java", "html", "css", "sql"]
+        found_skills = []
+
+        for skill in skills:
+            if skill.lower() in text.lower():
+                found_skills.append(skill)
+
+        # ⭐ score calculation
+        score = len(found_skills) * 20
+
+        if score >= 80:
+            message = "Excellent Resume 👍"
+        elif score >= 50:
+                message = "Good, but can improve 🙂"
+        else:
+                message = "Needs improvement ⚠️"
+
+        return render_template("result.html", skills=', '.join(found_skills), score=score)
     
     return "No file uploaded"
 
